74c7d6b7fe3187cc06a16ada3da2af4cde84dc77
csprh <paul.hill@bristol.ac.uk>
2018-09-29 22:44:45 +0100

    update

--- trainHAB.py
+++ trainHAB.py
@@ -141,7 +141,7 @@ def main(argv):
     """These are the main training settings. Set each before running
     this file."""
     # model can be one of lstm, mlp
-    mport pudb; pu.db
+    #import pudb; pu.db
 
     if (len(argv)==0):
         xmlName = 'classifyHAB1.xml'
