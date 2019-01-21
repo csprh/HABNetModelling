aa43a574f2348873870776247f67b4e52b08c13d
csprh <paul.hill@bristol.ac.uk>
2019-01-21 13:30:06 +0000

    update

--- trainHAB.py
+++ trainHAB.py
@@ -36,7 +36,7 @@ def train(inDir, dataDir,data_type, seqName, seq_length, model, image_shape,
     steps_per_epoch = (len(data.data) * 0.7) // batch_size
 
     #X, Y = data.get_all_sequences_in_memory('train', data_type)
-    X, Y, X_test, Y_test = data.get_all_sequences_in_memory2( data_type, 0.2)
+    X, Y, X_test, Y_test = data.get_all_sequences_in_memory2( data_type, 0.1)
 
     if model == 'svm':
         #tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-2, 1e-3, 1e-4, 1e-5],
