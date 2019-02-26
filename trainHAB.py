f54a58a6a34efc1c5438382866a2643155f43323
csprh <paul.hill@bristol.ac.uk>
2019-02-26 18:27:38 +0000

    update

--- trainHAB.py
+++ trainHAB.py
@@ -42,7 +42,7 @@ def train(inDir, dataDir,data_type, seqName, seq_length, model, image_shape,
     tb = TensorBoard(log_dir=os.path.join(dataDir, 'logs', model))
 
     # Helper: Stop when we stop learning.
-    early_stopper = EarlyStopping(monitor='val_acc', patience=2,  mode='auto')
+    early_stopper = EarlyStopping(monitor='val_acc', patience=500,  mode='auto')
 
     # Helper: Save results.
     timestamp = time.time()
