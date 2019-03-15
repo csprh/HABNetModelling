a2e5dfda2f7cbcce095b4db92a81b91a7ebf1640
csprh <paul.hill@bristol.ac.uk>
2019-03-15 23:35:52 +0000

    update8

--- HABDetectScript.py
+++ HABDetectScript.py
@@ -25,19 +25,18 @@ import testHAB
 import pudb; pu.db
 
 
-#h5name = '/home/cosc/csprh/linux/HABCODE/scratch/HAB/tmpTest/testCubes/Cube_09073_09081_737173.h5'
-#outputDirectory = '/home/cosc/csprh/linux/HABCODE/scratch/HAB/tmpTest/CNNIms'
+h5name = '/home/cosc/csprh/linux/HABCODE/scratch/HAB/tmpTest/testCubes/Cube_09073_09081_737173.h5'
+outputDirectory = '/home/cosc/csprh/linux/HABCODE/scratch/HAB/tmpTest/CNNIms'
 
 mstringApp = '/Applications/MATLAB_R2016a.app/bin/matlab'
 mstringApp = 'matlab'
 
-h5name = '/Users/csprh/Dlaptop/MATLAB/MYCODE/HAB/WORK/HAB/florida2/Cube_09073_09081_737173.h5'
-outputDirectory = '/Users/csprh/Dlaptop/MATLAB/MYCODE/HAB/WORK/HAB/CNNIms'
+#h5name = '/Users/csprh/Dlaptop/MATLAB/MYCODE/HAB/WORK/HAB/florida2/Cube_09073_09081_737173.h5'
+#outputDirectory = '/Users/csprh/Dlaptop/MATLAB/MYCODE/HAB/WORK/HAB/CNNIms'
 
 mstring = mstringApp + ' -nosplash -r \"outputImagesFromDataCubeScript ' +  h5name + ' ' + outputDirectory + '\"'
 os.system(mstring)
-extract_features.main('cnfgXMLs/NASNet11_lstm0.xml', outputDirectory)
-extract_features.main([])
+extract_features.main(['cnfgXMLs/NASNet11_lstm0.xml', outputDirectory])
 testHAB.main('cnfgXMLs/NASNet11_lstm0.xml', outputDirectory)
 
 
