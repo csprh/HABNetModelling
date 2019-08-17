a5eff085baed42692e2b48d6c15a20232a437494
csprh <paul.hill@bristol.ac.uk>
2019-08-17 14:44:28 +0100

    update

--- models.py
+++ models.py
@@ -131,7 +131,7 @@ class ResearchModels():
         # Model.
         model = Sequential()
         model.add(Bidirectional(LSTM(256, return_sequences=True, input_shape=self.input_shape)))
-        model.add(SeqSelfAttention(attention_activation='sigmoid'))
+        model.add(SeqSelfAttention(attention_activation='sigmoid',input_shape=self.input_shape))
         #model.add(LSTM(512, return_sequences=False, input_shape=self.input_shape))
         model.add(SeqSelfAttention(attention_activation='sigmoid'))
         #model.add(Dropout(0.5))
