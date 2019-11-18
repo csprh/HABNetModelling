7b7a69f3425e0cde1afc832d3d4b071e2d1b1adb
csprh <paul.hill@bristol.ac.uk>
2019-11-18 07:54:18 +0000

    update

--- RunAllTests.sh
+++ RunAllTests.sh
@@ -6,10 +6,10 @@
 #python3 trainHAB_CV.py ./cnfgXMLs/NASNet33_lstm1.xml
 #python3 trainHAB_CV.py ./cnfgXMLs/NASNet33_lstm2.xml
 #python3 trainHAB_CV.py ./cnfgXMLs/NASNet33_lstm3.xml
-python3 trainHAB_CV.py ./cnfgXMLs/NASNet33_lstm4.xml
+#python3 trainHAB_CV.py ./cnfgXMLs/NASNet33_lstm4.xml
 python3 trainHAB_CV.py ./cnfgXMLs/NASNet33_mlp0.xml
 python3 trainHAB_CV.py ./cnfgXMLs/NASNet33_mlp1.xml
-python3 trainHAB_CV.py ./cnfgXMLs/NASNet33_mlp2.xml
+#python3 trainHAB_CV.py ./cnfgXMLs/NASNet33_mlp2.xml
 #python3 trainHAB_CV.py ./cnfgXMLs/NASNet33_2_lstm0.xml
 #python3 trainHAB_CV.py ./cnfgXMLs/NASNet33_2_b_lstm0.xml
 #python3 trainHAB_CV.py ./cnfgXMLs/NASNet33_4_lstm0.xml
